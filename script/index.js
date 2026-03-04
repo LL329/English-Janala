7c0305367157c767967dce06145eee16fc4f6afa
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-03-04 23:20:57 +0600

    Show Synonyms using function and Manage Spinner

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
+const creatElements = (arr) => {
+  console.log(arr);
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join(" ");
+};
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
@@ -23,6 +29,34 @@ const loadLevelword = (id) => {
       displayWord(data.data);
     });
 };
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `<div class="">
+        <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">সমার্থক শব্দ গুলো</h2>
+        <div className="">${creatElements(word.synonyms)}</div>
+      </div>`;
+  document.getElementById("my_modal").showModal();
+};
 const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -47,9 +81,9 @@ const displayWord = (words) => {
             <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া জাইনি"}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
             <div>
-                <p class="text-2xl font-semibold font-bangla">"${word.meaning ? word.meaning : " অর্থ পাওয়া জাইনি"} / ${word.pronunciation ? word.pronunciatiion : "Pronunciation পাওয়া জাইনি"}"</p>
+                <p class="text-2xl font-semibold font-bangla">"${word.meaning ? word.meaning : " অর্থ পাওয়া জাইনি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া জাইনি"}"</p>
                 <div class="flex justify-between items-center">
-                    <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF20] hover:bg-[#1A91FF80]" ><i class="fa-solid fa-circle-info"></i></button>
+                    <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF20] hover:bg-[#1A91FF80]" ><i class="fa-solid fa-circle-info"></i></button>
 
                     <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
