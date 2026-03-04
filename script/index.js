3292a95ec26144a8dfc14bc072690f5ac5e7c700
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-03-04 20:43:50 +0600

    Handle Errors , empty lessons for Better UX

--- script/index.js
+++ script/index.js
@@ -15,15 +15,27 @@ const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length === 0) {
+    // alert("No Word Detected");
+    wordContainer.innerHTML = `
+     <div class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla">
+     <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-2xl font-medium text-gray-400  ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+    `;
+    return;
+  }
+
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
           <div class="bg-white rounded-xl shadow-lg py-10 px-5 space-y-5 text-center">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া জাইনি"}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
             <div>
-                <p class="text-2xl font-semibold font-bangla">"${word.meaning} / ${word.pronunciation}"</p>
+                <p class="text-2xl font-semibold font-bangla">"${word.meaning ? word.meaning : " অর্থ পাওয়া জাইনি"} / ${word.pronunciation ? word.pronunciatiion : "Pronunciation পাওয়া জাইনি"}"</p>
                 <div class="flex justify-between items-center">
                     <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF80]" ><i class="fa-solid fa-circle-info"></i></button>
 
@@ -55,3 +67,5 @@ const displayLesson = (lessons) => {
   }
 };
 loadLessons();
+
+// video 5 -> 9.14
